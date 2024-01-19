fdf7879bf5491cd1d6686e58fb57398e65ca71fb
simonjellvert <>
2024-01-19 11:37:31 +0000

    Customized admin

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,35 @@ from django.contrib import admin
 from .models import Category, Product, ProductContent
 
 
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(ProductContent)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'duration',
+        'online_onsite',
+        'price',
+    )
+
+    ordering = ('name',)
+
+
+class ProductContentAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'title',
+        'day',
+    )
+    
+    ordering = ('product',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductContent, ProductContentAdmin)
--- products/models.py
+++ products/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class Category(models.Model):
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -58,4 +61,4 @@ class ProductContent(models.Model):
     topics = models.TextField()
 
     def __str__(self):
-        return self.product.name
+        return f"{self.product.name if self.product else 'No Product'} - {self.title}"
