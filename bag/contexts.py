5a84808d26007d67f47f77b107d8fd5ca7f13809
Simon Jellvert <simon.jellvert@hotmail.com>
2024-01-24 12:34:11 +0000

    Add add to bag functionality

---
+++ bag/contexts.py
@@ -0,0 +1,18 @@
+from decimal import Decimal
+from django.conf import settings
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    product_count = 0
+    grand_total = 0
+    
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'grand_total': grand_total,
+    }
+
+    return context
--- nercia_webshop/settings.py
+++ nercia_webshop/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',   # Required
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
