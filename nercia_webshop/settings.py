a1c8398faed6b998d644a805dce9ae4a99e2f6bb
Simon Jellvert <simon.jellvert@hotmail.com>
2024-01-24 17:09:06 +0000

    Add toasts to base template

--- nercia_webshop/settings.py
+++ nercia_webshop/settings.py
@@ -80,6 +80,8 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = (
     # Needed to loginin by username in Django admin, regardless of 'allauth'
     'django.contrib.auth.backends.ModelBackend',
