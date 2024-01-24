01efa3e491f8686d4a4cdff415026af1002e3c5a
Simon Jellvert <simon.jellvert@hotmail.com>
2024-01-24 15:42:29 +0000

    Add functionalities to bag template

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_bag(request):
@@ -25,3 +25,34 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     return redirect(redirect_url)
 
+
+def edit_bag(request, item_id):
+    """
+    A view to edit quantity of a product to the bag
+    """
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+    
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def delete_item(request, item_id):
+    """
+    A view to delete item of a product to the bag
+    """
+    bag = request.session.get('bag', {})
+
+    try:
+        if item_id in bag:
+            bag.pop(item_id)
+        
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
