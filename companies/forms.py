6ce5921efaf80e52d34d5a5b234241b9e8f117b7
Simon Jellvert <simon.jellvert@hotmail.com>
2024-01-30 12:04:31 +0000

    Add crispy forms to profile template

--- companies/forms.py
+++ companies/forms.py
@@ -17,3 +17,25 @@ class CompanyForm(forms.ModelForm):
             'country',
             'invoice_email'
         ]
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'Company Name',
+            'org_num': 'Org. Number',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'postcode': 'Postal Code',
+            'city': 'Town or City',
+            'country': 'Country',
+            'invoice_email': 'Invoice Email',
+        }
+
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
--- profiles/forms.py
+++ profiles/forms.py
@@ -23,3 +23,20 @@ class UserProfileForm(forms.ModelForm):
             'last_name',
             'phone_number',
         ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'phone_number': 'Phone Number',
+        }
+
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
