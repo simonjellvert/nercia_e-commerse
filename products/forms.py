718e03b8649bb0fc6d9b0681d9615fef2063867a
Simon Jellvert <simon.jellvert@hotmail.com>
2024-02-08 10:25:34 +0000

    Edit product detail content

--- products/forms.py
+++ products/forms.py
@@ -44,6 +44,7 @@ class ProductForm(forms.ModelForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
 
+
 class ProductContentForm(forms.ModelForm):
     """
     Form for adding product content to the product
