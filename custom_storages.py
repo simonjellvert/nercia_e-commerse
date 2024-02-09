05387bea5a2e8cf7ebeec09b95a1ae5048d2c511
Simon Jellvert <simon.jellvert@hotmail.com>
2024-02-09 15:35:06 +0000

    Edit custom_storages file, typo

--- custom_storages.py
+++ custom_storages.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from storages.backends.s3boto3 import S3boto3Storage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3boto3Storage):
