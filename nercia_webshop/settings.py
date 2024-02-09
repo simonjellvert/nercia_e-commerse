eec80c2363008247d7669c255fbaefa3f78e6c94
Simon Jellvert <simon.jellvert@hotmail.com>
2024-02-09 15:48:11 +0000

    Edit settings, media folder not deployed to herokuoku

--- nercia_webshop/settings.py
+++ nercia_webshop/settings.py
@@ -194,7 +194,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    # Bucket config
+    # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'nercia-webshop'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -203,7 +203,7 @@ if 'USE_AWS' in os.environ:
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION ='static'
+    STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
